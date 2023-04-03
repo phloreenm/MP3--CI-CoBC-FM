f031d1b16cc2e58df9a767370c1178376444b8e2
Phlom <40276006+Phlom@users.noreply.github.com>
2023-04-03 01:02:07 +0100

    Refactor code to use TLS Certificate file instead of the Password(SCRAM) string

--- app.py
+++ app.py
@@ -1,31 +1,63 @@
+from pymongo import MongoClient
+from dotenv import load_dotenv
+from flask import Flask, render_template
 import os
-from flask import (
-    Flask, flash, render_template,
-    redirect, request, session, url_for)
-from flask_pymongo import PyMongo
-from bson.objectid import ObjectId
-from werkzeug.security import generate_password_hash, check_password_hash
-if os.path.exists("env.py"):
-    import env
+from env import *
+import logging
 
+load_dotenv()
 
 app = Flask(__name__)
 
-app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
-app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
-app.secret_key = os.environ.get("SECRET_KEY")
+if os.path.exists("env.py"):
+    import env
 
-mongo = PyMongo(app)
+client = MongoClient(MONGODB_URI, tls=True, tlsCertificateKeyFile=CERT_FILE_PATH)
+
+db = client[MONGO_DBNAME]
+users_coll = db['users']
+procedures_coll = db['procedures']
+daily_tasks_coll = db['daily_tasks']
 
 
 @app.route("/")
 @app.route("/index")
 def index():
-    tasks = list(mongo.db.tasks.find())
-    return render_template("index.html", tasks=tasks)
+    try:
+        users = list(users_coll.find())
+        for user in users:
+            print(f"User: {user}")
+        return render_template("index.html", users=users)
+    except Exception as e:
+        logging.error(f"Error accessing MongoDB: {e}", exc_info=True)
+        error_message = e
+        return render_template("error.html", error_message=error_message)
 
 
-if __name__ == "__main__":
-    app.run(host=os.environ.get("IP"),
-            port=int(os.environ.get("PORT")),
-            debug=True)
+@app.route("/procedures")
+def procedures():
+    try:
+        procedures = list(procedures_coll.find())
+        for procedure in procedures:
+            print(f"Procedure: {procedure}")
+        return render_template("procedures.html", procedures=procedures)
+    except Exception as e:
+        logging.error(f"Error accessing MongoDB: {e}", exc_info=True)
+        error_message = e
+        return render_template("error.html", error_message=error_message)
+
+
+@app.route("/tasks")
+def tasks():
+    tasks = list(daily_tasks_coll.find())
+    for task in tasks:
+        print(f"Task: {task}")
+    return render_template("tasks.html", tasks=tasks)
+
+
+if __name__ == '__main__':
+    app.run(
+        host=os.environ.get("IP"),
+        port=int(os.environ.get("PORT")),
+        debug=True
+    )
