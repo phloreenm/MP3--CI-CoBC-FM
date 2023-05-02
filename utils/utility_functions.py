b08718a35aecd398004921bbd6fa3511cdb8943f
Phlom <40276006+Phlom@users.noreply.github.com>
2023-05-02 14:34:58 +0100

    Add try/except error handling for default_admin function

--- utils/utility_functions.py
+++ utils/utility_functions.py
@@ -1,3 +1,5 @@
+from pymongo.errors import ConnectionFailure
+
 # if default admin user is not in the DB, then set variable to False
 default_admin_user_created = False
 
@@ -9,20 +11,24 @@ def check_sysadmin_user():
     if default_admin_user_created:
         return
     from app import users_coll
-    # Check if 'sysadmin' user already exists in the database
-    sysadmin = users_coll.find_one({'un': 'sysadmin'})
-    if sysadmin is not None:
-        return
-    # If 'sysadmin' user does not exist, create it with default password
-    hashed_password = generate_password_hash('1111')
-    new_user = {
-        'role': 'admin',
-        'un': 'sysadmin',
-        'pw': hashed_password,
-        'fname': 'System',
-        'lname': 'Admin',
-        'email': 'sysadmin@example.com',
-        'company': 'None'
-    }
-    users_coll.insert_one(new_user)
-    default_admin_user_created = True
+    try:
+        # Check if 'sysadmin' user already exists in the database
+        sysadmin = users_coll.find_one({'un': 'sysadmin'})
+        if sysadmin is not None:
+            return
+        # If 'sysadmin' user does not exist, create it with default password
+        hashed_password = generate_password_hash('1111')
+        new_user = {
+            'role': 'admin',
+            'un': 'sysadmin',
+            'pw': hashed_password,
+            'fname': 'System',
+            'lname': 'Admin',
+            'email': 'sysadmin@example.com',
+            'company': 'None'
+        }
+        users_coll.insert_one(new_user)
+        default_admin_user_created = True
+    except (ConnectionFailure, Exception) as e:
+        flash(
+            'Unable to connect to the database: ' + str(e), 'error')
